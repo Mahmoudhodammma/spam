fd1ab73e6f0d6b768ce4396513da476873bb7efb
Mahmoudhodammma <91755885+Mahmoudhodammma@users.noreply.github.com>
2022-01-21 17:30:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "5212904097:AAH8f8jPVM3-p8WP-fvAjc6FeNRfX6HJFKs"
-APP_URL = "https://bot7amody.herokuapp.com/" + BOT_TOKEN
+BOT_TOKEN = "5078982797:AAFTZgvYyE0qDO3uATjB00XyGn_aYV0pOUM"
+APP_URL = "https://spambotpy.herokuapp.com/" + BOT_TOKEN
