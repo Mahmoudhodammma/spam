0212c5c0c32016e991ba1f021d199bd0ab50b443
Mahmoudhodammma <91755885+Mahmoudhodammma@users.noreply.github.com>
2022-01-21 17:34:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -46,5 +46,5 @@ def redirect_message():
 
 if __name__ == "__main__":
     bot.remove_webhook()
-    bot.set_webhook(url="https://bot7amody.herokuapp.com/"+str(BOT_TOKEN))
+    bot.set_webhook(url="https://spambotpy.herokuapp.com/"+str(BOT_TOKEN))
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
